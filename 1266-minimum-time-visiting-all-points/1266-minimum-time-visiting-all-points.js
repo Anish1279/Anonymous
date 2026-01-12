2770a174cbf0e3953aa027aa072a7c5d0ac80977
Anish1279 <117334096+Anish1279@users.noreply.github.com>
2026-01-12 14:38:04 +0530

    Time: 0 ms (100%), Space: 54.5 MB (91.4%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
@@ -0,0 +1,10 @@
+var minTimeToVisitAllPoints = function(p) {
+    let ans = 0;
+    for (let i = 1; i < p.length; i++) {
+        ans += Math.max(
+            Math.abs(p[i][0] - p[i - 1][0]),
+            Math.abs(p[i][1] - p[i - 1][1])
+        );
+    }
+    return ans;
+};
