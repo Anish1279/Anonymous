61989b4c500f57b7bca5c65056ef73a7472c50b5
Anish1279 <117334096+Anish1279@users.noreply.github.com>
2026-02-04 17:09:55 +0530

    Time: 0 ms (100%), Space: 56.2 MB (41.39%) - LeetHub

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} apple
+ * @param {number[]} capacity
+ * @return {number}
+ */
+const minimumBoxes = (apple, cap) => {
+    let sum = apple.reduce((a, c) => a + c, 0);    
+    cap.sort((a, b) => b - a);
+    
+    let res = 0;
+    while (sum > 0)
+        sum -= cap[res++];
+    
+    return res;
+};
