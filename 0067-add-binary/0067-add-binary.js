42cb8917bee62429c8ff5ad657575c56d184313c
Anish1279 <117334096+Anish1279@users.noreply.github.com>
2026-02-15 10:41:46 +0530

    Time: 2 ms (41.77%), Space: 54.9 MB (66.78%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+const addBinary = (a, b) => {
+    const max = Math.max(a.length, b.length);
+    let carry = 0;
+    let res = ``;
+
+    const sum = (A, B, C) => (A ^ B) ^ C;
+    const carr = (A, B, C) => (A & B) | ((A ^ B) & C);
+
+    for (let i = 0; i < max; i++) {
+        const A = +(a.at(-1 - i) ?? 0);
+        const B = +(b.at(-1 - i) ?? 0);
+        res = `${sum(A, B, carry)}${res}`;
+        carry = carr(A, B, carry);
+    }
+
+    return (carry || ``) + res
+};
